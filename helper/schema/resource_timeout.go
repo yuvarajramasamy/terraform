c1e6024810832333385b7e1117a723399944f4cf
clint shryock <clint@ctshryock.com>
2017-02-03 09:18:04 -0600

    the timeout thing

---
+++ helper/schema/resource_timeout.go
@@ -0,0 +1,147 @@
+package schema
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/mitchellh/copystructure"
+)
+
+func DefaultTimeout(tx time.Duration) *time.Duration {
+	return &tx
+}
+
+type ResourceTimeout struct {
+	Create, Read, Update, Delete, Default *time.Duration
+}
+
+// ConfigDecode takes a schema and the configuration (available in Diff) and
+// validates, parses the timeouts into `t`
+func (t *ResourceTimeout) ConfigDecode(s *Resource, c *terraform.ResourceConfig) error {
+
+	log.Printf("\n@@@\nResource timeouts: %s", spew.Sdump(s.Timeouts))
+	log.Printf("\n@@@\nConfig timeouts: %s", spew.Sdump(c.Config["timeout"]))
+
+	if s.Timeouts != nil {
+		raw, cerr := copystructure.Copy(s.Timeouts)
+		if cerr != nil {
+			log.Printf("\n@@@\nError with deep copy: %s\n@@@\n", cerr)
+		}
+		// type assertion
+		// rnt := raw.(ResourceTimeout)
+		// t = &rnt
+		t = raw.(*ResourceTimeout)
+	}
+
+	// if s.Timeouts != nil {
+	// 	t.Create = s.Timeouts.Create
+	// 	t.Update = s.Timeouts.Create
+	// 	t.Read = s.Timeouts.Create
+	// 	t.Delete = s.Timeouts.Create
+	// 	t.Default = s.Timeouts.Create
+	// }
+
+	log.Printf("what is T now: %s", spew.Sdump(t))
+
+	if v, ok := c.Config["timeout"]; ok {
+		raw := v.([]map[string]interface{})
+		// raw is []map[string]interface{}
+		for _, tv := range raw {
+			log.Printf("\n***\n rawT %s", spew.Sdump(tv))
+			// rawT := tv.(map[string]interface{})
+			for mk, mv := range tv {
+				log.Printf("\n$$$$ inner kv: %s // %s", mk, mv.(string))
+				keys := []string{"create", "read", "update", "delete", "default"}
+				var found bool
+				for _, key := range keys {
+					if mk == key {
+						found = true
+						break
+					}
+				}
+
+				if found {
+					log.Printf("\n*** found %s", mk)
+				}
+
+				if !found {
+					return fmt.Errorf("Unsupported timeout key found (%s)", mk)
+				}
+
+				if mk == "create" && t.Create == nil {
+					return fmt.Errorf("Timeout (%s) is not supported", mk)
+				} else {
+					log.Printf("\n***\nOverwrote (%s)", mk)
+					rt, err := time.ParseDuration(mv.(string))
+					if err != nil {
+						return fmt.Errorf("Error parsing Timeout for (%s): %s", mk, err)
+					}
+					t.Create = &rt
+					continue
+				}
+
+				if mk == "read" && t.Read == nil {
+					return fmt.Errorf("Timeout (%s) is not supported", mk)
+				} else {
+					log.Printf("\n***\nOverwrote (%s)", mk)
+					rt, err := time.ParseDuration(mv.(string))
+					if err != nil {
+						return fmt.Errorf("Error parsing Timeout for (%s): %s", mk, err)
+					}
+					t.Read = &rt
+					continue
+				}
+
+				if mk == "update" && t.Update == nil {
+					return fmt.Errorf("Timeout (%s) is not supported", mk)
+				} else {
+					log.Printf("\n***\nOverwrote (%s)", mk)
+					rt, err := time.ParseDuration(mv.(string))
+					if err != nil {
+						return fmt.Errorf("Error parsing Timeout for (%s): %s", mk, err)
+					}
+					t.Update = &rt
+					continue
+				}
+
+				if mk == "delete" && t.Delete == nil {
+					return fmt.Errorf("Timeout (%s) is not supported", mk)
+				} else {
+					log.Printf("\n***\nOverwrote (%s)", mk)
+					rt, err := time.ParseDuration(mv.(string))
+					if err != nil {
+						return fmt.Errorf("Error parsing Timeout for (%s): %s", mk, err)
+					}
+					t.Delete = &rt
+					continue
+				}
+
+				if mk == "default" && t.Default == nil {
+					return fmt.Errorf("Timeout (%s) is not supported", mk)
+				} else {
+					log.Printf("\n***\nOverwrote (%s)", mk)
+					rt, err := time.ParseDuration(mv.(string))
+					if err != nil {
+						return fmt.Errorf("Error parsing Timeout for (%s): %s", mk, err)
+					}
+					t.Default = &rt
+					continue
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+// MetaEncode and MetaDecode are analogous to the Go stdlib JSONEncoder
+// interface: they encode/decode a timeouts struct from an instance diff, which is
+// where the timeout data is stored after a diff to pass into Apply.
+//
+// MetaEncode called in Step #2
+// MetaDecode called in Step #4
+func (t *ResourceTimeout) MetaEncode(*terraform.InstanceDiff) error { return nil }
+func (t *ResourceTimeout) MetaDecode(*terraform.InstanceDiff) error { return nil }
